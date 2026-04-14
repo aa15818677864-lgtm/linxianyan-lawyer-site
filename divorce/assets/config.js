9ac808a4d35e67194c2302fe5f8374d95c442a9c
Kentar <aa15989267747@gmail.com>
2026-04-14 15:59:39 +0800

    Add standalone divorce landing page

---
+++ divorce/assets/config.js
@@ -0,0 +1,6 @@
+window.LAWYER_SITE_CONFIG = {
+  lawyerName: "林先妍",
+  lawFirm: "",
+  siteLabel: "林先妍律師離婚專題落地頁",
+  formEndpoint: "https://script.google.com/macros/s/AKfycbwxisd_6AMUqMrmdgVCoo29A42q4vendgfzRnM-A_XQjeBZx41Hu1TXa9LpcKKV7r7z/exec"
+};
