58319d8c8124151627b28a3fdf444b4b1d67eab3
Kentar <aa15989267747@gmail.com>
2026-04-14 16:09:25 +0800

    Refine divorce page for Google Ads compliance

--- divorce/assets/config.example.js
+++ divorce/assets/config.example.js
@@ -1,6 +1,6 @@
 window.LAWYER_SITE_CONFIG = {
   lawyerName: "林先妍",
   lawFirm: "",
-  siteLabel: "林先妍律師離婚專題落地頁",
+  siteLabel: "林先妍律師跨境婚姻程序專題",
   formEndpoint: "https://script.google.com/macros/s/REPLACE_WITH_DEPLOYMENT_ID/exec"
 };
--- divorce/assets/config.js
+++ divorce/assets/config.js
@@ -1,6 +1,6 @@
 window.LAWYER_SITE_CONFIG = {
   lawyerName: "林先妍",
   lawFirm: "",
-  siteLabel: "林先妍律師離婚專題落地頁",
+  siteLabel: "林先妍律師跨境婚姻程序專題",
   formEndpoint: "https://script.google.com/macros/s/AKfycbwxisd_6AMUqMrmdgVCoo29A42q4vendgfzRnM-A_XQjeBZx41Hu1TXa9LpcKKV7r7z/exec"
 };
