fca6491d8fbea8b216a7582007c05f952b733d14
Kentar <aa15989267747@gmail.com>
2026-04-15 11:26:24 +0800

    Refine criminal topic messaging

--- criminal/assets/config.example.js
+++ criminal/assets/config.example.js
@@ -1,6 +1,6 @@
 window.LAWYER_SITE_CONFIG = {
   lawyerName: "林先妍",
   lawFirm: "",
-  siteLabel: "林先妍律師內地刑事案件專題",
+  siteLabel: "林先妍律師內地刑事案件初步釐清專題",
   formEndpoint: "https://script.google.com/macros/s/REPLACE_WITH_DEPLOYMENT_ID/exec"
 };
