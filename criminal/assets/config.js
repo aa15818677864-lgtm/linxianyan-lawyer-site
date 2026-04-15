9c434a0b4c33700b66cd46c474d83e49daefcf88
Kentar <aa15989267747@gmail.com>
2026-04-15 11:49:59 +0800

    Upgrade criminal topic conversion copy

--- criminal/assets/config.js
+++ criminal/assets/config.js
@@ -1,6 +1,6 @@
 window.LAWYER_SITE_CONFIG = {
   lawyerName: "林先妍",
   lawFirm: "",
-  siteLabel: "林先妍律師內地刑事案件初步釐清專題",
+  siteLabel: "林先妍律師內地刑事案件方向釐清專題",
   formEndpoint: "https://script.google.com/macros/s/AKfycbwxisd_6AMUqMrmdgVCoo29A42q4vendgfzRnM-A_XQjeBZx41Hu1TXa9LpcKKV7r7z/exec"
 };
