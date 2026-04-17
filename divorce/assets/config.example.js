c8fecd7a2953c887611b982d5217850113736d5b
Kentar <aa15989267747@gmail.com>
2026-04-17 15:59:06 +0800

    Refocus divorce topic and fix nav hover contrast

--- divorce/assets/config.example.js
+++ divorce/assets/config.example.js
@@ -1,6 +1,6 @@
 window.LAWYER_SITE_CONFIG = {
   lawyerName: "林先妍",
   lawFirm: "",
-  siteLabel: "林先妍律師跨境婚姻程序專題",
+  siteLabel: "林先妍律師內地與香港離婚及財產分割專頁",
   formEndpoint: "https://script.google.com/macros/s/REPLACE_WITH_DEPLOYMENT_ID/exec"
 };
